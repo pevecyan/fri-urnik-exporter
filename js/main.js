46054e8e3caf0fc89f87a573e431b4d59019f5a4
Žan Pevec <pevec@outlook.com>
2016-09-23 23:35:00 +0200

    d

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var friScheduleExporter = require('fri-schedule-exporter');
 
-friScheduleExporter.getSchedule("2015_2016_zimski", "63130185", function(data,err){
+friScheduleExporter.getScheduleFromBrowser("2015_2016_zimski", "63130185", function(data,err){
     var i = 0;
     console.log(data);
 });
